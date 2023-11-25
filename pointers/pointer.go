84fb0c6d37e92312836308d4d41c71bf7a7b6e60
Mahesh <mahesh.jandwani@acko.tech>
2023-11-25 23:47:47 +0530

    Basics of Pointers

---
+++ pointers/pointer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+
+}
+
+func (w *Wallet) Balance() int {
+
+	return w.balance
+}
