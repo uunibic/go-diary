51488b5a11ea249fb3f53cdf33780f1c1afa783d
Mahesh <mahesh.jandwani@acko.tech>
2023-11-26 20:33:57 +0530

    Basics of Map

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Below is the list of resources I've utilized to develop proficiency in the Go pr
 
 - Learn go with tests : https://quii.gitbook.io
 - Golang Guru (My Own CustomGPT) : https://chat.openai.com/g/g-yI3O2E1AO-golang-guru
+- Gophercises : https://gophercises.com/
+- Go Excercises : https://golangr.com/exercises
---
+++ maps/dictionary_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestDict(t *testing.T) {
+
+	assertStrings := func(t testing.TB, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
